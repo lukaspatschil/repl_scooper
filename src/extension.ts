67693eaf17d8e80af8af8c96c1371357e9717200
lukas spatschil <lukas@spatschil.at>
2020-10-15 19:53:42 +0200

    old changes

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { getWebviewContnet } from './webview/screen';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "replcode" is now active!');
@@ -13,14 +13,28 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('replcode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand(
+		'replcode.helloWorld',
+		() => {
+			// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from REPLCode!');
+			// Display a message box to the user
+			vscode.window.showInformationMessage('Hello World from REPLCode!');
+		}
+	);
+
+	let webview = vscode.commands.registerCommand('replcode.window', () => {
+		const panel = vscode.window.createWebviewPanel(
+			'replWebview',
+			'REPL Test',
+			vscode.ViewColumn.Two,
+			{}
+		);
+
+		panel.webview.html = getWebviewContnet();
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(webview);
 }
 
 // this method is called when your extension is deactivated
