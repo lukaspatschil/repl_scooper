d214e965a5fd8a31c9cb6fa31228bc19daaa2d1e
lukas spatschil <lukas@spatschil.at>
2021-01-20 20:14:42 +0100

    fixed types

--- src/view/app/hooks/useCode.tsx
+++ src/view/app/hooks/useCode.tsx
@@ -7,7 +7,7 @@ export const useCode = (code: any, global: any) => {
   const [estree, setEstree] = useState(code);
   const [variables, setVariables] = useState(code?.params);
   const [globals, setGlobals] = useState(global);
-  const [generated, setGenerated] = useState<string>(undefined);
+  const [generated, setGenerated] = useState<string>("");
   const [output, setOutput] = useState<any>(undefined);
 
   useEffect(() => {
@@ -17,9 +17,9 @@ export const useCode = (code: any, global: any) => {
   }, [estree]);
 
   const updateOutput = () => {
-    const values = [];
+    const values: Array<any> = [];
 
-    variables.forEach(el => values.push(el?.value));
+    variables.forEach((el: { value: any; }) => values.push(el?.value));
 
     const ast = make_global(globals);
 
@@ -29,6 +29,7 @@ export const useCode = (code: any, global: any) => {
     const func = new Function(`${global_string}return ${generated}`)();
 
     try {
+      // @ts-ignore
       setOutput(Reflect.apply(func, undefined, values));
     } catch (err) {
       console.log(err);
@@ -37,7 +38,7 @@ export const useCode = (code: any, global: any) => {
   };
 
   const setVariable = (name: string, value: any) => {
-    const variable = variables.find(el => el.name === name);
+    const variable = variables.find((el: { name: string; }) => el.name === name);
 
     if (variable) {
       variable.value = value;
@@ -47,7 +48,7 @@ export const useCode = (code: any, global: any) => {
   };
 
   const setGlobal = (name: string, value: any) => {
-    const variable = globals.find(el => el?.declarations[0]?.id?.name === name);
+    const variable = globals.find((el: { declarations: { id: { name: string; }; }[]; }) => el?.declarations[0]?.id?.name === name);
 
     if (variable?.declarations[0]?.init) {
       variable.declarations[0].init.value = value;
