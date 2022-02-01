1e0e9c9565f40299c3f69ea33509c64791c87c92
Lukas Spatschil <lukas@spatschil.at>
2022-02-01 16:27:04 +0100

    Remove generated file

--- .vscode/generated.js
+++
@@ -1,8 +0,0 @@
-
-const test123 = "a";
-const testasd = 1;
-
-function name2(params) {
-  return test123.filter(el => el === 'a');
-}
-Promise.resolve(name2(1)).then(data => console.log(data)).catch(error => console.log(error));
